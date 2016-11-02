c95d7ee3e45b5b3f27b39ce0e67458e35262053d
MsXuemeiWang <MsXuemeiWang@DESKTOP-P0IG5TA>
2016-11-02 19:55:41 -0400

    Add project files.

---
+++ COMP229-Lesson8/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace COMP229_Lesson8
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ COMP229-Lesson8/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="COMP229_Lesson8.MvcApplication" Language="C#" %>
